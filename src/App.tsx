50fca0772a1efd8d3d3ebcf20fb1c9e6b9e0abe4
Saikrishnadas <saikrishnadas666@gmail.com>
2021-06-24 11:18:13 +0530

    handling type functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React,{useState} from 'react';
+import "./App.css";
 import List from './components/List';
 
 interface IState {
@@ -13,10 +14,17 @@ interface IState {
 
 
 function App() {
-const [people,setPeople] = useState<IState["people"]>([])
+const [people,setPeople] = useState<IState["people"]>([
+  {
+    name:"Ronaldo",
+    age:36,
+    url:"https://phantom-marca.unidadeditorial.es/3cddf31fc0fa7f65567f202bf98c7e89/resize/1320/f/jpg/assets/multimedia/imagenes/2021/06/16/16238262149819.jpg",
+    note: "Top Scorer in Euros"
+  }
+])
 
   return (
-    <div>
+    <div className="App">
       <h1>Party Invited List </h1>
       <List people={people}/>
     </div>
