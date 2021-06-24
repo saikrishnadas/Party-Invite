128d931093ee84964374c453d3fe84e76f457d03
Saikrishnadas <saikrishnadas666@gmail.com>
2021-06-24 10:55:45 +0530

    adding types to state and passing it as props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React,{useState} from 'react';
+import List from './components/List';
+
+interface IState {
+  people: {
+    name:string,
+    age:number,
+    url:string,
+    note?:string
+  }[]
+}
+
 
 
 function App() {
+const [people,setPeople] = useState<IState["people"]>([])
 
   return (
     <div>
       <h1>Party Invited List </h1>
+      <List people={people}/>
     </div>
   );
 }
---
+++ src/components/List.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface IProps {
+    people: {
+      name:string,
+      age:number,
+      url:string,
+      note?:string
+    }[]
+  }
+
+const List: React.FC<IProps> = ({people}) => {
+    return <div>I am a list</div> 
+}
+
+export default List;
